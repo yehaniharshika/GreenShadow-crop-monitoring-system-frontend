dad44f07577dfcc5ebf2fd2a903a1104b4a32396
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-08 16:46:48 +0530

    create toggle.js

---
+++ assets/js/toggle.js
@@ -0,0 +1,31 @@
+const sidebar = document.getElementById("sidebar");
+const menuToggle = document.getElementById("menu-toggle");
+const menuToggleField = document.getElementById("menu-toggle1");
+const menuToggleStaff = document.getElementById("menu-toggle2");
+const menuToggleCrop = document.getElementById("menu-toggle3");
+const menuToggleLogs = document.getElementById("menu-toggle4");
+const closeSidebar = document.getElementById("close-sidebar");
+
+menuToggle.addEventListener("click", () => {
+    sidebar.classList.add("active");
+});
+
+menuToggleField.addEventListener("click", () =>{
+    sidebar.classList.add("active");
+});
+
+menuToggleStaff.addEventListener("click", () =>{
+    sidebar.classList.add("active");
+});
+
+menuToggleCrop.addEventListener("click", () =>{
+    sidebar.classList.add("active");
+});
+
+menuToggleLogs.addEventListener("click", () =>{
+    sidebar.classList.add("active");
+});
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
